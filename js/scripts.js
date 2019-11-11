d693370096cf36e9d62ab312c6e3026ba8495f06
Mburiah <bykieswagix5@gmail.com>
2019-11-11 10:37:17 +0300

    functions for pizza order

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
+    return parseInt(selectedValue);
+}
+function getCrust() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getTopping() {
+    var selectedToppings = document.getElementById("topping").value;
+    return parseInt(selectedToppings);
+}
+function getQuantity() {
+    var selectedQuantity = document.getElementById("quantity").value;
+    return parseInt(selectedQuantity);
+}
+function getTotalAmount() {
+    var totalAmount = (getSizeValue() + getCrust() + getTopping()) * getQuantity();
+    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    prompt("Enter your location")
+    alert("You order will be delivered shortly.Thanks  for shopping with us. We are here for you everytime!")
+}
